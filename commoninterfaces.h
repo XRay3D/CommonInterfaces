e06069b3883694061d4a47d3236e60a1ee47fad2
Дамир Бакиев <bakiev@elemer.org>
2021-10-01 10:33:02 +0300

    m_connected -> connected_

--- commoninterfaces.h
+++ commoninterfaces.h
@@ -6,12 +6,12 @@ class CommonInterfaces {
 
 public:
     virtual bool ping([[maybe_unused]] const QString& portName, [[maybe_unused]] int baud, [[maybe_unused]] int addr) = 0;
-    virtual bool isConnected() const { return m_connected; }
-    virtual void reset() { m_connected = false; }
+    virtual bool isConnected() const { return connected_; }
+    virtual void reset() { connected_ = false; }
 
     virtual void open(int mode) = 0;
     virtual void close() = 0;
 
 protected:
-    bool m_connected = false;
+    bool connected_ = false;
 };
